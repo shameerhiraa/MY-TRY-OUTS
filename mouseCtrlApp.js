72a21ab1353e8c1528ef4aba27de4ca8574b0aa5
shameerhiraa <shemimp@gmail.com>
2017-03-12 00:23:16 +0530

    test

---
+++ mouseCtrlApp.js
@@ -0,0 +1,29 @@
+var mgApp = angular.module('mgApp',["ngRoute"]);
+//var theApp = angular.module("theApp",[])
+mgApp.run(function($rootScope,$location){
+	$rootScope.stages = ['00','01','02','03'];
+	$rootScope.currentStageIdx = 0;
+	
+	$rootScope.increaseOneStep = function(){
+		$rootScope.currentStageIdx++;
+		console.log($rootScope.stages[$rootScope.currentStageIdx])
+		$location.path($rootScope.stages[$rootScope.currentStageIdx])
+	}
+})
+
+mgApp.config(function($routeProvider){
+	//console.log('route')
+	$routeProvider.
+	when("/00",{
+		templateUrl:'views/00.html',
+		controller:'startSceneCtrl'
+	}).when("/01",{
+		templateUrl:'views/01.html',
+		controller:'startSceneCtrl'
+	}).otherwise({redirectTo:'00'})
+})
+
+
+mgApp.controller('startSceneCtrl',function($scope,$rootScope,$location){
+	
+})
