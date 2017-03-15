08da83587fea8a067a6c130b89d666fe56e7282b
shameerhiraa <shemimp@gmail.com>
2017-03-15 18:24:19 +0530

    test

--- mouseCtrlApp.js
+++ mouseCtrlApp.js
@@ -6,8 +6,8 @@ mgApp.run(function($rootScope,$location){
 	
 	$rootScope.increaseOneStep = function(){
 		$rootScope.currentStageIdx++;
-		console.log($rootScope.stages[$rootScope.currentStageIdx])
-		$location.path($rootScope.stages[$rootScope.currentStageIdx])
+		//console.log($rootScope.stages[$rootScope.currentStageIdx])
+		$location.path($rootScope.stages[1])
 	}
 })
 
@@ -19,7 +19,7 @@ mgApp.config(function($routeProvider){
 		controller:'startSceneCtrl'
 	}).when("/01",{
 		templateUrl:'views/01.html',
-		controller:'startSceneCtrl'
+		controller:'firstStageCtrl'
 	}).otherwise({redirectTo:'00'})
 })
 
@@ -28,7 +28,53 @@ mgApp.controller('startSceneCtrl',function($scope,$rootScope,$location){
 	
 })
 
-mgApp.directive('pathDir',function(){
+mgApp.controller('firstStageCtrl',function($scope){
+	
+})
+
+
+mgApp.directive('pathCanv',function(){
+	
+	function theLinkFunction(sc,el,attr){
+		
+		
+		
+		var img = new Image();
+		
+		function onCanvasMouseMove(e){
+			
+			var img_data = $(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[3]
+			console.log(img_data)
+		}
+		function onCanvasMouseEnter(e){
+			
+			//var img_data = $(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[3]
+			console.log('mouseover')
+		}
+		
+		img.onload = function(){
+			console.log('onload success')
+			var canv = $(el).get(0).getContext("2d").drawImage(img,0,0,1024,760)
+			$(el).on('mousemove',onCanvasMouseMove);
+			$(el).on('mouseover',onCanvasMouseEnter)
+		}
+		img.onerror = function(){
+			
+			console.log('error')
+		}
+		
+		
+		
+		img.src = 'images/stage1.svg';
+		
+	}
+	
+	
+	return{
+		
+		link:theLinkFunction
+		
+	}
 	
 	
 	
