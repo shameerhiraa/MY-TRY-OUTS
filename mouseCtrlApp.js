b074e195cc0a9129e47095ba5dd0307baaebb715
shameerhiraa <shemimp@gmail.com>
2017-03-16 17:25:57 +0530

    TEST

--- mouseCtrlApp.js
+++ mouseCtrlApp.js
@@ -3,6 +3,7 @@ var mgApp = angular.module('mgApp',["ngRoute"]);
 mgApp.run(function($rootScope,$location){
 	$rootScope.stages = ['00','01','02','03'];
 	$rootScope.currentStageIdx = 0;
+	$rootScope.isPlaying = false;
 	
 	$rootScope.increaseOneStep = function(){
 		$rootScope.currentStageIdx++;
@@ -43,8 +44,31 @@ mgApp.directive('pathCanv',function(){
 		
 		function onCanvasMouseMove(e){
 			
-			var img_data = $(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[3]
-			console.log(img_data)
+			var alpha = $(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[3]
+			//console.log(alpha)
+			var hex = $(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[0].toString(16)+$(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[1].toString(16)+$(el).get(0).getContext("2d").getImageData(e.offsetX,e.offsetY,1,1).data[2].toString(16);
+
+			//console.log(sc.isPlaying)
+			
+			//console.log(alpha)
+			console.log(hex)
+			if(sc.isPlaying && alpha == 0){
+				sc.isPlaying = false;
+				console.log('FAILED')
+				
+			}
+			
+			if(hex == '7afea' && !sc.isPlaying){
+				sc.isPlaying = true;
+				console.log('Started')
+				
+				
+			}
+			
+			if((hex == 'c692'|| hex == '0660')&& sc.isPlaying){
+				sc.isPlaying = false;
+				console.log('WON THE STAGE')
+			}
 		}
 		function onCanvasMouseEnter(e){
 			
